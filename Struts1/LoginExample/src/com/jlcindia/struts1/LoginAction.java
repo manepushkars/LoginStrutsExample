a90c4397200bdfe87abced600dfd94b9d8abe0a8
manepushkars <30971700+manepushkars@users.noreply.github.com>
2020-07-22 20:36:31 +0530

    Add files via upload
    Initial commit

---
+++ Struts1/LoginExample/src/com/jlcindia/struts1/LoginAction.java
@@ -0,0 +1,26 @@
+package com.jlcindia.struts1;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class LoginAction extends Action{
+	String result = "";
+	@Override
+	public ActionForward execute(ActionMapping am, ActionForm af, HttpServletRequest req,
+			HttpServletResponse res) throws Exception {
+		LoginForm lf = (LoginForm) af;
+		String un = lf.getUsername();
+		String pw = lf.getPassword();
+		if(un.equals(pw)) {
+			result = "success";
+		}
+		ActionForward forward = am.findForward(result);
+				return forward;
+		
+	}
+}
